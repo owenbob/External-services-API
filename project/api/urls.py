abbd31597b09f0de1a22a91733c823282a45dfb4
owenbob <obyomuhangi@gmail.com>
2018-10-26 10:18:44 +0300

    [Ft #161440703]Add CRUD for external services
    This commit add CRUD for JSONplaceholder external service via the posts route. It also add listing a user and retrieving a user

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,10 +1,18 @@
 """
 Module containing all api app url routes
 """
-from django.conf.urls import url
-from api.views import SetUp
+from django.conf.urls import url, include
+from rest_framework import routers
+
+from api.views import SetUp, ListUsers, RetrieveUser, PostsViewSet
+
+router = routers.SimpleRouter()
+router.register(r'posts', PostsViewSet, base_name="posts")
 
 urlpatterns = [
-    url(r'^api/', SetUp.as_view(), name="default")
+    url(r'^$', SetUp.as_view(), name="setup"),
+    url(r'^users/', ListUsers.as_view(), name="users"),
+    url(r'^retrieve/(?P<pk>[0-9]+)', RetrieveUser.as_view(), name="retrieve_user"),
+    url(r'^', include(router.urls))
 ]
 
--- project/api/views.py
+++ project/api/views.py
@@ -1,7 +1,12 @@
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework import status
+import requests
 
 # Create your views here.
+USER_BASE_URL = 'https://jsonplaceholder.typicode.com/users'
+POSTS_BASE_URL = 'https://jsonplaceholder.typicode.com/posts'
 
 class SetUp(APIView):
     """
@@ -12,3 +17,70 @@ class SetUp(APIView):
         message = " You have succesfully setup Django 1.11 and DRF"
         return Response(message)
 
+
+class ListUsers(APIView):
+    """
+    Class to list all users from the JsonPlaceHolder  external API
+    Reference: 'https://jsonplaceholder.typicode.com/users'
+    """
+    def get(self, request):
+        response = requests.get(USER_BASE_URL)
+        if response.status_code == 200:
+            return Response(response.json())
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class RetrieveUser(APIView):
+    """
+    class to retrieve specified users from JsonPlaceHolder  external API
+    Reference: 'https://jsonplaceholder.typicode.com/users'
+    """
+    def get(self, request, pk):
+        response = requests.get(USER_BASE_URL+"/"+pk)
+        if response.status_code == 200:
+            return Response(response.json())
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+class PostsViewSet(viewsets.ViewSet):
+    """
+    Class to hadnle interaction with post from the JsonPlaceHolder external API
+    reference: 'https://jsonplaceholder.typicode.com/users' 
+    """
+    def list(self, request):
+        response = requests.get(POSTS_BASE_URL)
+        if response.status_code == 200:
+            return Response(response.json())
+        return response(status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk):
+        response = requests.get(POSTS_BASE_URL+"/"+pk+"/")
+        if response.status_code == 200:
+            return Response(response.json())
+        return response(status=status.HTTP_404_NOT_FOUND)
+
+    def create(self, request):
+        payload = {
+            "title": request.data.get("title"),
+            "body": request.data.get("body"),
+            "userId": request.data.get("user_id")
+        }
+        response = requests.post(POSTS_BASE_URL,data=payload)
+        if response.status_code == 201:
+            return Response(response.json(), status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk):
+        payload = {
+            "title": request.data.get("title"),
+            "body": request.data.get("body"),
+            "userId": request.data.get("user_id")
+        }
+        response = requests.put(POSTS_BASE_URL+"/"+pk,data=payload)
+        if response.status_code == 200:
+            return Response(response.json(), status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def destroy(self, request, pk):
+        response = requests.put(POSTS_BASE_URL+"/"+pk)
+        if response.status_code == 200:
+            return Response(response.json(), status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_404_NOT_FOUND)
