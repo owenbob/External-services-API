c4668aa40ae00d29ccbf74d8860cac1b6ca6122d
owenbob <30471763+owenbob@users.noreply.github.com>
2018-11-02 08:45:41 +0300

    Merge dev work (#5)
    * [Ft #161440703]Add CRUD for external services (#2)
    
    This commit add CRUD for JSONplaceholder external service via the posts route. It also add listing a user and retrieving a user
    
    * [ch #161440708]Add tests (#3)
    
    * Ft intergrate elastic search 161440717 (#4)
    
    * [feature #161440717] Integrate Elastic search
    
    This commit integrates elasticsearch so that every post is indexed as soon as it is created.
    
    * Add travis
    
    * Add travis
    
    * Add travis
    
    * Add travis
    
    * Add circleci config

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,10 +1,19 @@
 """
 Module containing all api app url routes
 """
-from django.conf.urls import url
-from api.views import SetUp
+from django.conf.urls import url, include
+from rest_framework import routers
+
+from api.views import SetUp, ListUsers, RetrieveUser, PostsViewSet, ElasticSearch
+
+router = routers.SimpleRouter()
+router.register(r'posts', PostsViewSet, base_name="posts")
 
 urlpatterns = [
-    url(r'^api/', SetUp.as_view(), name="default")
+    url(r'^$', SetUp.as_view(), name="setup"),
+    url(r'^users/', ListUsers.as_view(), name="users"),
+    url(r'^search/', ElasticSearch.as_view(),name="search"),
+    url(r'^retrieve/(?P<pk>[0-9]+)', RetrieveUser.as_view(), name="retrieve_user"),
+    url(r'^', include(router.urls))
 ]
 
--- project/api/views.py
+++ project/api/views.py
@@ -1,7 +1,16 @@
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework import status
+import requests
+
+from api.serializers import PostsSerializer
+from api.models import Posts
+from api.search import search
 
 # Create your views here.
+USER_BASE_URL = 'https://jsonplaceholder.typicode.com/users'
+POSTS_BASE_URL = 'https://jsonplaceholder.typicode.com/posts'
 
 class SetUp(APIView):
     """
@@ -12,3 +21,95 @@ class SetUp(APIView):
         message = " You have succesfully setup Django 1.11 and DRF"
         return Response(message)
 
+class ElasticSearch(APIView):
+    """
+    Class to do a simple search using elastic search
+    """
+    def get(self, request):
+        q = request.query_params.get("q")
+        if q:
+            response = search(q)
+            res = [{"title":b.title,"body":b.body,"id":b.meta.id} for b  in response.hits]
+            data = {
+                "results":res,
+                "results_count":len(res)
+            }
+            return Response(data)
+
+
+class ListUsers(APIView):
+    """
+    Class to list all users from the JsonPlaceHolder  external API
+    Reference: 'https://jsonplaceholder.typicode.com/users'
+    """
+    def get(self, request):
+        response = requests.get(USER_BASE_URL)
+        if response.status_code == 200:
+            return Response(response.json())
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class RetrieveUser(APIView):
+    """
+    class to retrieve specified users from JsonPlaceHolder  external API
+    Reference: 'https://jsonplaceholder.typicode.com/users'
+    """
+    def get(self, request, pk):
+        response = requests.get(USER_BASE_URL+"/"+pk)
+        if response.status_code == 200:
+            return Response(response.json())
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+class PostsViewSet(viewsets.ViewSet):
+    """
+    Class to handle interaction with post from the JsonPlaceHolder external API
+    reference: 'https://jsonplaceholder.typicode.com/users' 
+    """
+
+    def list(self, request):
+        response = requests.get(POSTS_BASE_URL)
+        if response.status_code == 200:
+            return Response(response.json())
+        return response(status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk):
+        response = requests.get(POSTS_BASE_URL+"/"+pk+"/")
+        if response.status_code == 200:
+            return Response(response.json())
+        return response(status=status.HTTP_404_NOT_FOUND)
+
+    def create(self, request):
+
+        title = request.data.get("title")
+        body = request.data.get("body")
+        
+        payload = {
+            "title": title,
+            "body": body
+        }
+
+        post = PostsSerializer(data=payload)
+
+        if post.is_valid():
+            post.save()
+
+        response = requests.post(POSTS_BASE_URL,data=payload)
+        if response.status_code == 201:
+            return Response(response.json(), status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk):
+        payload = {
+            "title": request.data.get("title"),
+            "body": request.data.get("body"),
+            "userId": request.data.get("user_id")
+        }
+        response = requests.put(POSTS_BASE_URL+"/"+pk,data=payload)
+        if response.status_code == 200:
+            return Response(response.json(), status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def destroy(self, request, pk):
+        response = requests.put(POSTS_BASE_URL+"/"+pk)
+        if response.status_code == 200:
+            return Response(response.json(), status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_404_NOT_FOUND)
